20f1480e805ca5e8efb58bf8dfd545fb0248b9fe
Dmitry Sidorov <jonmagon@gmail.com>
2020-09-09 19:20:35 +0300

    Update localization

--- data/translations/kdiskmark_de_DE.ts
+++ data/translations/kdiskmark_de_DE.ts
@@ -72,31 +72,31 @@
 <context>
     <name>Benchmark</name>
     <message>
-        <location filename="../../src/benchmark.cpp" line="180"/>
-        <location filename="../../src/benchmark.cpp" line="190"/>
+        <location filename="../../src/benchmark.cpp" line="196"/>
+        <location filename="../../src/benchmark.cpp" line="206"/>
         <source>Sequential Read %1/%2</source>
         <translation>Sequenzielles Lesen %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="185"/>
-        <location filename="../../src/benchmark.cpp" line="195"/>
+        <location filename="../../src/benchmark.cpp" line="201"/>
+        <location filename="../../src/benchmark.cpp" line="211"/>
         <source>Sequential Write %1/%2</source>
         <translation>Sequenzielles Schreiben %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="200"/>
-        <location filename="../../src/benchmark.cpp" line="210"/>
+        <location filename="../../src/benchmark.cpp" line="216"/>
+        <location filename="../../src/benchmark.cpp" line="226"/>
         <source>Random Read %1/%2</source>
         <translation>Zufälliges Lesen %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="205"/>
-        <location filename="../../src/benchmark.cpp" line="215"/>
+        <location filename="../../src/benchmark.cpp" line="221"/>
+        <location filename="../../src/benchmark.cpp" line="231"/>
         <source>Random Write %1/%2</source>
         <translation>Zufälliges Schreiben %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="221"/>
+        <location filename="../../src/benchmark.cpp" line="237"/>
         <source>Interval Time %1/%2 sec</source>
         <translation>Pause zwischen Überprüfungen %1/%2 sec</translation>
     </message>
--- data/translations/kdiskmark_ru_RU.ts
+++ data/translations/kdiskmark_ru_RU.ts
@@ -72,31 +72,31 @@
 <context>
     <name>Benchmark</name>
     <message>
-        <location filename="../../src/benchmark.cpp" line="180"/>
-        <location filename="../../src/benchmark.cpp" line="190"/>
+        <location filename="../../src/benchmark.cpp" line="196"/>
+        <location filename="../../src/benchmark.cpp" line="206"/>
         <source>Sequential Read %1/%2</source>
         <translation>Последовательное чтение %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="185"/>
-        <location filename="../../src/benchmark.cpp" line="195"/>
+        <location filename="../../src/benchmark.cpp" line="201"/>
+        <location filename="../../src/benchmark.cpp" line="211"/>
         <source>Sequential Write %1/%2</source>
         <translation>Последовательная запись %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="200"/>
-        <location filename="../../src/benchmark.cpp" line="210"/>
+        <location filename="../../src/benchmark.cpp" line="216"/>
+        <location filename="../../src/benchmark.cpp" line="226"/>
         <source>Random Read %1/%2</source>
         <translation>Случайное чтение %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="205"/>
-        <location filename="../../src/benchmark.cpp" line="215"/>
+        <location filename="../../src/benchmark.cpp" line="221"/>
+        <location filename="../../src/benchmark.cpp" line="231"/>
         <source>Random Write %1/%2</source>
         <translation>Случайная запись %1/%2</translation>
     </message>
     <message>
-        <location filename="../../src/benchmark.cpp" line="221"/>
+        <location filename="../../src/benchmark.cpp" line="237"/>
         <source>Interval Time %1/%2 sec</source>
         <translation>Пауза между тестами %1/%2 сек</translation>
     </message>
