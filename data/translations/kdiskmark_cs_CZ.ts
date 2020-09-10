3ac9b4ff32ff96bc8a66a25c6330fd481214ae40
p-bo <pavel.borecki@gmail.com>
2020-09-10 10:51:35 +0200

    Add Czech translation

---
+++ data/translations/kdiskmark_cs_CZ.ts
@@ -0,0 +1,441 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../../src/about.ui" line="17"/>
+        <source>About KDiskMark</source>
+        <translation>O KDiskMark</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="62"/>
+        <source>Version:</source>
+        <translation>Verze:</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="88"/>
+        <source>License:</source>
+        <translation>Licence:</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="101"/>
+        <source>GPL v3</source>
+        <translation>GPL v3</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="114"/>
+        <source>Country:</source>
+        <translation>Země:</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="127"/>
+        <source>Russia</source>
+        <translation>Rusko</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="140"/>
+        <source>Author:</source>
+        <translation>Autor:</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="153"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/JonMagon&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;JonMagon&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/JonMagon&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;JonMagon&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="172"/>
+        <source>E-mail:</source>
+        <translation>E-mail:</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="185"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;mailto:jonmagon@gmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;jonmagon@gmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;mailto:jonmagon@gmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;jonmagon@gmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="204"/>
+        <source>Flexible I/O Tester:</source>
+        <translation>Flexible I/O Tester:</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="230"/>
+        <source>Application Icon:</source>
+        <translation>Ikona aplikace:</translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="243"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://www.iconfinder.com/baitisstudio&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;Baiti Studio&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://www.iconfinder.com/baitisstudio&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;Baiti Studio&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Benchmark</name>
+    <message>
+        <location filename="../../src/benchmark.cpp" line="169"/>
+        <location filename="../../src/benchmark.cpp" line="185"/>
+        <source>Sequential Read</source>
+        <translation>Sekvenční čení</translation>
+    </message>
+    <message>
+        <location filename="../../src/benchmark.cpp" line="177"/>
+        <location filename="../../src/benchmark.cpp" line="193"/>
+        <source>Sequential Write</source>
+        <translation>Sekvenční zápis</translation>
+    </message>
+    <message>
+        <location filename="../../src/benchmark.cpp" line="201"/>
+        <location filename="../../src/benchmark.cpp" line="217"/>
+        <source>Random Read</source>
+        <translation>Náhodné čtení</translation>
+    </message>
+    <message>
+        <location filename="../../src/benchmark.cpp" line="209"/>
+        <location filename="../../src/benchmark.cpp" line="225"/>
+        <source>Random Write</source>
+        <translation>Náhodný zápis</translation>
+    </message>
+    <message>
+        <location filename="../../src/benchmark.cpp" line="235"/>
+        <source>Interval Time %1/%2 sec</source>
+        <translation>Prodleva mezi testy %1/%2 sek</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="14"/>
+        <source>KDiskMark</source>
+        <translation>KDiskMark</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="30"/>
+        <location filename="../../src/mainwindow.cpp" line="417"/>
+        <source>All</source>
+        <translation>Vše</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="108"/>
+        <location filename="../../src/mainwindow.ui" line="158"/>
+        <location filename="../../src/mainwindow.ui" line="228"/>
+        <location filename="../../src/mainwindow.ui" line="252"/>
+        <location filename="../../src/mainwindow.ui" line="289"/>
+        <location filename="../../src/mainwindow.ui" line="313"/>
+        <location filename="../../src/mainwindow.ui" line="350"/>
+        <location filename="../../src/mainwindow.ui" line="387"/>
+        <source>%v</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="188"/>
+        <source>MB/s</source>
+        <translation>MB/s</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="193"/>
+        <source>GB/s</source>
+        <translation>GB/s</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="198"/>
+        <source>IOPS</source>
+        <translation>IOPS</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="203"/>
+        <source>μs</source>
+        <translation>μs</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="134"/>
+        <source>SEQ 1</source>
+        <translation>SEQ 1</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="265"/>
+        <source>SEQ 2</source>
+        <translation>SEQ 2</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="326"/>
+        <source>RND 1</source>
+        <translation>NHD 1</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="363"/>
+        <source>RND 2</source>
+        <translation>NHD 1</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="420"/>
+        <source>File</source>
+        <translation>Soubor</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="428"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="432"/>
+        <source>Interval Time</source>
+        <translation>Prodleva mezi testy</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="450"/>
+        <source>Profile</source>
+        <translation>Profil</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="455"/>
+        <source>Help</source>
+        <translation>Nápověda</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="470"/>
+        <source>0 sec</source>
+        <translation>0 sek</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="478"/>
+        <source>1 sec</source>
+        <translation>1 sek</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="486"/>
+        <source>3 sec</source>
+        <translation>3 sec</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="497"/>
+        <source>5 sec</source>
+        <translation>5 sek</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="505"/>
+        <source>10 sec</source>
+        <translation>10 sec</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="513"/>
+        <source>30 sec</source>
+        <translation>30 sek</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="521"/>
+        <source>1 min</source>
+        <translation>1 min</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="529"/>
+        <source>3 min</source>
+        <translation>3 min</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="537"/>
+        <source>5 min</source>
+        <translation>5 min</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="545"/>
+        <source>10 min</source>
+        <translation>10 min</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="550"/>
+        <source>About KDiskMark</source>
+        <translation>O KDiskMark</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="555"/>
+        <source>Queues &amp;&amp; Threads</source>
+        <translation>Fronty a vlákna</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="560"/>
+        <source>Copy</source>
+        <translation>Kopírovat</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="565"/>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="570"/>
+        <source>Exit</source>
+        <translation>Konec</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="67"/>
+        <location filename="../../src/mainwindow.cpp" line="244"/>
+        <source>MiB</source>
+        <translation>MiB</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="112"/>
+        <location filename="../../src/mainwindow.cpp" line="138"/>
+        <source>%1 %2% (%3)</source>
+        <translation>%1 %2% (%3)</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="201"/>
+        <location filename="../../src/mainwindow.cpp" line="207"/>
+        <source>&lt;h2&gt;Sequential %1 MiB&lt;br/&gt;Queues=%2&lt;br/&gt;Threads=%3&lt;/h2&gt;</source>
+        <translation>&lt;h2&gt;Sekvenční %1 MiB&lt;br/&gt;Front=%2&lt;br/&gt;Vláken=%3&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="213"/>
+        <location filename="../../src/mainwindow.cpp" line="219"/>
+        <source>&lt;h2&gt;Random %1 KiB&lt;br/&gt;Queues=%2&lt;br/&gt;Threads=%3&lt;/h2&gt;</source>
+        <translation>&lt;h2&gt;Náhodné %1 KiB&lt;br/&gt;Front=%2&lt;br/&gt;Vláken=%3&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="244"/>
+        <source>Bytes</source>
+        <translation>Bajtů</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="244"/>
+        <source>KiB</source>
+        <translation>KiB</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="71"/>
+        <location filename="../../src/mainwindow.cpp" line="244"/>
+        <source>GiB</source>
+        <translation>GiB</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="244"/>
+        <source>TiB</source>
+        <translation>TiB</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="244"/>
+        <source>PiB</source>
+        <translation>PiB</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="121"/>
+        <location filename="../../src/mainwindow.cpp" line="262"/>
+        <source>Read</source>
+        <translation>Čtení</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="171"/>
+        <location filename="../../src/mainwindow.cpp" line="265"/>
+        <source>Write</source>
+        <translation>Zápis</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="444"/>
+        <source>Stopping...</source>
+        <translation>Zastavování…</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="448"/>
+        <source>Not available</source>
+        <translation>Není k dispozici</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="448"/>
+        <source>Directory is not specified.</source>
+        <translation>Není zadána složka.</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="451"/>
+        <source>Confirmation</source>
+        <translation>Potvrzení</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="452"/>
+        <source>This action destroys the data in %1
+Do you want to continue?</source>
+        <translation>Touto akci budou přepsána data v %1.
+Chcete pokračovat?</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="465"/>
+        <source>Benchmark Failed</source>
+        <translation>Test se nezdařil</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="399"/>
+        <location filename="../../src/mainwindow.cpp" line="400"/>
+        <location filename="../../src/mainwindow.cpp" line="401"/>
+        <location filename="../../src/mainwindow.cpp" line="402"/>
+        <location filename="../../src/mainwindow.cpp" line="403"/>
+        <source>Stop</source>
+        <translation>Stop</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/main.cpp" line="53"/>
+        <source>No FIO was found. Please install FIO before using KDiskMark.</source>
+        <translation>FIO nenalezeno. Aby bylo možné používat KDiskMark, je třeba nainstalovat FIO.</translation>
+    </message>
+    <message>
+        <location filename="../../src/global.cpp" line="13"/>
+        <source>&lt;h1&gt;%1 MB/s&lt;br/&gt;%2 GB/s&lt;br/&gt;%3 IOPS&lt;br/&gt;%4 μs&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;%1 MB/s&lt;br/&gt;%2 GB/s&lt;br/&gt;%3 IOPS&lt;br/&gt;%4 μs&lt;/h1&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../../src/settings.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.ui" line="29"/>
+        <source>Sequential (1)</source>
+        <translation>Sekvenční (1)</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.ui" line="85"/>
+        <source>Sequential (2)</source>
+        <translation>Sekvenční (2)</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.ui" line="151"/>
+        <source>Random (1)</source>
+        <translation>Náhodné (1)</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.ui" line="167"/>
+        <source>Random (2)</source>
+        <translation>Náhodné (2)</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.ui" line="213"/>
+        <source>Block Size</source>
+        <translation>Velikost bloku</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.ui" line="229"/>
+        <source>Queues</source>
+        <translation>Front</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.ui" line="245"/>
+        <source>Threads</source>
+        <translation>Vláken</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.cpp" line="27"/>
+        <location filename="../../src/settings.cpp" line="28"/>
+        <source>MiB</source>
+        <translation>MiB</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings.cpp" line="41"/>
+        <location filename="../../src/settings.cpp" line="42"/>
+        <source>KiB</source>
+        <translation>KiB</translation>
+    </message>
+</context>
+</TS>
