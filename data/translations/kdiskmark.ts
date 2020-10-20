076604c5fe807df36833def008eb00a0329bfd22
Dmitry Sidorov <jonmagon@gmail.com>
2020-10-20 20:13:44 +0300

    Update source localization file

--- data/translations/kdiskmark.ts
+++ data/translations/kdiskmark.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<TS version="2.1">
 <context>
     <name>About</name>
     <message>
@@ -21,7 +21,7 @@
     <message>
         <location filename="../../src/about.ui" line="101"/>
         <source>GPL v3</source>
-        <translation></translation>
+        <translation>GPL v3</translation>
     </message>
     <message>
         <location filename="../../src/about.ui" line="114"/>
@@ -41,7 +41,7 @@
     <message>
         <location filename="../../src/about.ui" line="153"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/JonMagon&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;JonMagon&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/JonMagon&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;JonMagon&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../src/about.ui" line="172"/>
@@ -51,12 +51,12 @@
     <message>
         <location filename="../../src/about.ui" line="185"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;mailto:jonmagon@gmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;jonmagon@gmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;mailto:jonmagon@gmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;jonmagon@gmail.com&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../src/about.ui" line="204"/>
         <source>Flexible I/O Tester:</source>
-        <translation></translation>
+        <translation>Flexible I/O Tester:</translation>
     </message>
     <message>
         <location filename="../../src/about.ui" line="230"/>
@@ -66,7 +66,7 @@
     <message>
         <location filename="../../src/about.ui" line="243"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://www.iconfinder.com/baitisstudio&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;Baiti Studio&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://www.iconfinder.com/baitisstudio&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;Baiti Studio&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -118,7 +118,7 @@
     <message>
         <location filename="../../src/mainwindow.ui" line="14"/>
         <source>KDiskMark</source>
-        <translation></translation>
+        <translation>KDiskMark</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="30"/>
@@ -129,28 +129,28 @@
     <message>
         <location filename="../../src/mainwindow.ui" line="43"/>
         <source>Test 1</source>
-        <translation></translation>
+        <translation>Test 1</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="56"/>
         <source>Test 2</source>
-        <translation></translation>
+        <translation>Test 2</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="69"/>
         <source>Test 3</source>
-        <translation></translation>
+        <translation>Test 3</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="82"/>
         <source>Test 4</source>
-        <translation></translation>
+        <translation>Test 4</translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="177"/>
         <location filename="../../src/mainwindow.cpp" line="338"/>
         <source>Read</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="207"/>
@@ -172,13 +172,13 @@
         <location filename="../../src/mainwindow.ui" line="337"/>
         <location filename="../../src/mainwindow.cpp" line="341"/>
         <source>Write</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="497"/>
         <location filename="../../src/mainwindow.cpp" line="344"/>
         <source>Mix</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/mainwindow.ui" line="702"/>
