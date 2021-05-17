1959e1edab5efefd29a9a65094fd8c40a1d8b2a4
Dmitry Sidorov <jonmagon@gmail.com>
2021-05-17 19:24:41 +0300

    Fix #52
    Co-authored-by: Alexey Dokuchaev <danfe@FreeBSD.org>

--- src/diskdriveinfo.cpp
+++ src/diskdriveinfo.cpp
@@ -3,6 +3,10 @@
 #include <QString>
 #include <QFile>
 #include <QFileInfo>
+#ifdef __FreeBSD__
+#include <sys/disk.h>
+#include <sys/fcntl.h>
+#endif
 
 QString DiskDriveInfo::getDeviceByVolume(const QString &volume)
 {
@@ -12,6 +16,7 @@ QString DiskDriveInfo::getDeviceByVolume(const QString &volume)
 
 QString DiskDriveInfo::getModelName(const QString &volume)
 {
+#if defined(__linux__)
     QFileInfo sysClass(QFileInfo(QString("/sys/class/block/%1/..")
                                  .arg(getDeviceByVolume(volume)))
                        .canonicalFilePath());
@@ -21,9 +26,23 @@ QString DiskDriveInfo::getModelName(const QString &volume)
     if (!sysBlock.open(QIODevice::ReadOnly | QIODevice::Text))
         return QString();
 
-    QString model = sysBlock.readAll().simplified();
+    QString model(sysBlock.readAll().simplified());
 
     sysBlock.close();
+#elif defined(__FreeBSD__)
+    struct diocgattr_arg arg;
+
+    strlcpy(arg.name, "GEOM::descr", sizeof(arg.name));
+    arg.len = sizeof(arg.value.str);
+
+    int fd = open(volume.toStdString().c_str(), O_RDONLY);
+    if (fd == -1 || ioctl(fd, DIOCGATTR, &arg) == -1)
+        return QString();
+
+    QString model(arg.value.str);
+
+    close(fd);
+#endif
 
     return model;
 }
